3db737f48062a31ff8124c2cf1dc4ae8d71188d5
Adriel Klem <adrielklem16@gmail.com>
2024-10-11 14:04:11 -0300

    fix export Lista de Alunos

---
+++ ListaAlunos.py
@@ -0,0 +1,13 @@
+from ClassListaAlunos import Aluno
+
+Alunos = []
+
+Alunos.append(Aluno("Bruno",10))
+Alunos.append(Aluno("Adriel",8))
+Alunos.append(Aluno("Rapha",7))
+Alunos.append(Aluno("Ana",9))
+Alunos.append(Aluno("Duda",6))
+Alunos.append(Aluno("Miguel",4))
+Alunos.append(Aluno("Carlos",3))
+
+__all__ = ["Alunos"]
